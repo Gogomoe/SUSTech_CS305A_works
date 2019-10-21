c4b3ae03128970fa934be8bd2704daaf7267c8ec
Gogo <gogo81745.moe@gmail.com>
2019-10-21 14:10:13 +0800

    lab6 fix auto range

--- Lab06/WebFileBrowser.py
+++ Lab06/WebFileBrowser.py
@@ -188,19 +188,8 @@ def handleRange(request: Request, respond: Respond) -> Respond:
     respond_status, respond_header, body = respond
     file_size = len(body)
 
-    if 'range' not in header and len(body) <= 1024 * 1024:
+    if 'range' not in header:
         return respond
-
-    if 'range' not in header and len(body) > 1024 * 1024:
-        range_from = 0
-        range_to = 1024 * 1024 - 1
-
-        respond_status = (206, 'Partial Content')
-        respond_header['Content-Range'] = 'bytes {}-{}/{}'.format(str(range_from), str(range_to),
-                                                                  str(file_size))
-        respond_header['Content-Length'] = str(range_to - range_from + 1)
-        body = body[range_from:range_to + 1]
-        return respond_status, respond_header, body
 
     try:
         range = header['range']
@@ -208,11 +197,10 @@ def handleRange(request: Request, respond: Respond) -> Respond:
         end_pos = range.index('-')
 
         range_from = int(range[start_pos + 1:end_pos])
-        range_to = file_size - 1
+        range_to: int
 
         if '-' == range[-1]:
-            if range_to - range_from + 1 > 4 * 1024 * 1024:
-                range_to = range_from + 4 * 1024 * 1024 - 1
+            range_to = file_size - 1
         else:
             range_to = int(range[end_pos + 1:])
             range_to = min(file_size - 1, range_to)
